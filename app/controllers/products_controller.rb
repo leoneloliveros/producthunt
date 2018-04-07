1c0f99427b6dac3c5436b54add4a8e7649f40280
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-04-06 19:13:40 -0500

    Create route patch /products/:id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,14 @@ class ProductsController < ApplicationController
   def edit
     @product = Product.find(params[:id])
   end
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path, notice: "El producto se ha modificado con exito"
+    else
+      render :edit
+    end
+  end
   private
     def product_params
       params.require(:product).permit(:name, :url, :description)
